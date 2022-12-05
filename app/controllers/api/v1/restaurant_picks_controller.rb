0edd842e84e0c81dfe0f6d13797ced3dc26fdbf8
Henry Yeh <heyeh@icloud.com>
2022-12-05 17:22:59 +0800

    restaurant pick controller SHOW updated

--- app/controllers/api/v1/restaurant_picks_controller.rb
+++ app/controllers/api/v1/restaurant_picks_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::RestaurantPicksController < ApplicationController
+  def show
+    @restaurant_pick = RestaurantPick.find(params[:id])
+    render json: { restaurant_pick: @restaurant_pick}
+  end
 end
