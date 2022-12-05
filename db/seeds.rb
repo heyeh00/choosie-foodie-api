6eb295ecec51c35fbf02f9c90e347fba7b197357
Henry Yeh <heyeh@icloud.com>
2022-12-05 17:48:23 +0800

    seeds for 100 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "destroying all restaurants"
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.street_address} Shanghai",
+    cuisine: Faker::Restaurant.type,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    ave_price: [100..500].sample,
+    rating: [1..5].sample
+  )
+  puts restaurant.name
+end
+
+puts "Restaurants created!"
