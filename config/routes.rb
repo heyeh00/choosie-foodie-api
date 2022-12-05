e3bcc2f19327617ba743e05fe6f26505a52f8b85
Henry Yeh <heyeh@icloud.com>
2022-12-05 15:26:29 +0800

    added events to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'login', to: 'sessions#login', as: :login
+      resources :events, only: %i[index create show result]
     end
   end
 end
