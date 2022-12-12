e9de0f409627d4bf5d6a3fb99f2febdb79ea7176
Henry Yeh <95343944+heyeh00@users.noreply.github.com>
2022-12-12 15:59:08 +0800

    updated restaurant picks to create only if event is not closed

--- app/controllers/api/v1/restaurant_picks_controller.rb
+++ app/controllers/api/v1/restaurant_picks_controller.rb
@@ -2,15 +2,20 @@ class Api::V1::RestaurantPicksController < Api::V1::BaseController
   skip_before_action :verify_request
 
   def create
-    restaurants = set_restaurants
-    restaurants.each do |restaurant|
-      @restaurant_pick = RestaurantPick.create(
-        user: User.find(params[:user_id]),
-        event: Event.find(params[:event_id]),
-        event_restaurant: EventRestaurant.find(restaurant)
-      )
+    @event = Event.find(params[:event_id])
+    unless @event.closed
+      restaurants = set_restaurants
+      restaurants.each do |restaurant|
+        @restaurant_pick = RestaurantPick.create(
+          user: User.find(params[:user_id]),
+          event: Event.find(params[:event_id]),
+          event_restaurant: EventRestaurant.find(restaurant)
+        )
+      end
+      render json: { restaurant_pick: @restaurant_pick }
+    else
+      render json: { restaurant_pick: }
     end
-    render json: { restaurant_pick: @restaurant_pick }
   end
 
   private
