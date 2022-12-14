a7b7d5483d46f2e9e21a73eea4c834f8d1cde037
Henry Yeh <95343944+heyeh00@users.noreply.github.com>
2022-12-14 14:12:57 +0800

    user dependent destroys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :events
-  has_many :restaurant_picks
+  has_many :events, dependent: :destroy
+  has_many :restaurant_picks, dependent: :destroy
   has_one_attached :avatar
   # has_many :restaurant_picks, through: :events
 end
